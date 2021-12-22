7ff4184318ca13412094bb283d17176a6218a101
j-panchenko <il586419001@gmail.com>
2021-12-22 14:26:42 +0200

    add API Service

--- src/components/pages/meal/daily-meal-plan.js
+++ src/components/pages/meal/daily-meal-plan.js
@@ -1,10 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './daily-meal-plan.css';
+import FatsecretService from '../../../services/fatsecret';
 
-const DailyMealPlan = () => (
-  <section>
-    <h2 className="section-title">Your daily meal plan</h2>
-  </section>
-);
+class DailyMealPlan extends Component {
+  service = new FatsecretService();
+
+  constructor() {
+    super();
+    this.state = {
+      food: {
+        foodName: '',
+        calories: 0,
+        servingSize: 0,
+        protein: 0,
+        fat: 0,
+        carbohydrates: 0,
+      },
+    };
+  }
+
+  onFoodAdded = (food) => {
+    this.setState({ food });
+  };
+
+  addFood = () => {
+    const { food: { foodName } } = this.state;
+    this.service.getResultsOfSearch(foodName)
+      .then((food) => {
+        this.onFoodAdded(food[0]);
+        console.log(food);
+      });
+  };
+
+  onHandleChange = (e) => {
+    this.setState({ food: { foodName: e.target.value } });
+  };
+
+  onHandleSubmit = (e) => {
+    e.preventDefault();
+    this.addFood();
+  };
+
+  render() {
+    const { food } = this.state;
+    return (
+      <section>
+        <h2 className="section-title">Your daily meal plan</h2>
+        <form onSubmit={this.onHandleSubmit}>
+          <label htmlFor="search-food">
+            What did you eat today?
+            <input type="text" id="search-food" onChange={this.onHandleChange} />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+        <ul>
+          <li>
+            Calories:
+            {' '}
+            {food.calories}
+          </li>
+          <li>
+            Serving size:
+            {' '}
+            {food.servingSize}
+          </li>
+          <li>
+            Protein:
+            {' '}
+            {food.protein}
+          </li>
+          <li>
+            Fat:
+            {' '}
+            {food.fat}
+          </li>
+          <li>
+            Carbohydrates:
+            {' '}
+            {food.carbohydrates}
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
 
 export default DailyMealPlan;
---
+++ src/services/fatsecret.js
@@ -0,0 +1,49 @@
+// import axios from 'axios';
+
+export default class FatsecretService {
+  apiBase = 'https://api.api-ninjas.com/v1/nutrition?query=';
+
+  options = {
+    method: 'GET',
+    headers: {
+      'X-Api-Key': 'Q4mNprkESFMH4gJ7FOmZEA==KkKmfyHx82G0suVk',
+    },
+  };
+
+  onParsedString = (str) => {
+    const newString = str.split(' ').join('%20');
+    return newString;
+  };
+
+  getResource = async (food) => {
+    const requestedFood = this.onParsedString(food);
+    const result = await fetch(`${this.apiBase}${requestedFood}`, this.options);
+
+    if (!result.ok) {
+      throw new Error(`Could not fetch ${this.apiBase}, received ${result.status}`);
+    }
+
+    const res = await result.json();
+
+    return res;
+  };
+
+  getResultsOfSearch = async (food) => {
+    const res = await this.getResource(food);
+    // console.log(res);
+    return res.map((item) => this.transformFood(item));
+  };
+
+  transformFood = (food) => {
+    const result = {
+      foodName: food.name,
+      calories: food.calories,
+      servingSize: `${food.serving_size_g} g`,
+      protein: food.protein_g,
+      fat: food.fat_total_g,
+      carbohydrates: food.carbohydrates_total_g,
+    };
+    console.log(result);
+    return result;
+  };
+}
