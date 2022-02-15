2d385605a0391e799310e98cca0a1430e5401c9e
j-panchenko <il586419001@gmail.com>
2022-02-15 19:16:29 +0200

    instead of BrowserRouter use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ErrorBoundary from './components/error-boundary';
 import './index.css';
@@ -15,9 +15,9 @@ ReactDOM.render(
   <Provider store={store}>
     <ErrorBoundary>
       <NinjasServiceProvider value={ninjasService}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </NinjasServiceProvider>
     </ErrorBoundary>
   </Provider>,
