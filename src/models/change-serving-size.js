2050c546a19965902c319158390f3158b1ee86ff
j-panchenko <il586419001@gmail.com>
2022-02-19 22:26:04 +0200

    add parseFloat func for convert string

--- src/models/change-serving-size.js
+++ src/models/change-serving-size.js
@@ -1,6 +1,6 @@
 const changeServingSize = (endSize, seekOut) => {
   const servingSize = 100;
-  return Number((endSize * seekOut) / servingSize).toFixed(1);
+  return parseFloat(Number((endSize * seekOut) / servingSize).toFixed(1));
 };
 
 export default changeServingSize;
